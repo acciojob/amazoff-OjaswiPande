eebc0bfd90ea78cd293c3132feb0e805f0c8274c
OjaswiPande <ojaswipande@gmail.com>
2025-03-14 03:14:34 +0530

    Complete The assignment

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,8 +9,16 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id=id;
+        this.deliveryTime=convertTimeToMinutes(deliveryTime);
     }
-
+    int convertTimeToMinutes(String time) {
+        String[] parts = time.split(":"); // Splitti
+        // ng HH:MM
+        int hours = Integer.parseInt(parts[0]); // Extract HH
+        int minutes = Integer.parseInt(parts[1]); // Extract MM
+        return hours * 60 + minutes; // Convert to total minutes
+    }
     public String getId() {
         return id;
     }
